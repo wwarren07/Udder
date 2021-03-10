540b13d29c3ef37d1d4205e293578ba4d10cf559
William Warren <wwarren07@gmail.com>
2021-03-10 08:25:43 -0700

    add view func to view controller

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -21,6 +21,10 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+    }
 
 
 }
